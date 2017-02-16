c8951cebc993d140ba45b1ab6e244185dc47a6b5
Zhuoqi Li <George@Zhuoqis-MacBook-Pro.local>
2017-02-16 12:39:07 -0800

    Update

--- Project2/solution.py
+++
@@ -1,1 +0,0 @@
-from sklearn.datasets import fetch_20newsgroupscategories = ['comp.graphics']graphics_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
