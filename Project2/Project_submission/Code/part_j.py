1d3890ed33e2b5aaeb7ae48532e9a51c6eca07f8
Zhuoqi Li <George@Zhuoqis-MacBook-Pro.local>
2017-02-16 12:34:07 -0800

    project 2 Final update

--- Project2/Project_submission/Code/part_j.py
+++ Project2/Project_submission/Code/part_j.py
@@ -4,7 +4,7 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.preprocessing import Normalizer
-
+from sklearn.naive_bayes import GaussianNB
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -44,15 +44,14 @@ def TFIDF_transform(categories,train_or_test,stop_words,NLTK_StopWords,count_vec
     return twenty_data, X_tfidf, X_counts
 
 if __name__ == "__main__":
-    categories = [ 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
-    'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
+    categories = ['comp.sys.ibm.pc.hardware' , 'comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']
 
     stop_words, NLTK_StopWords = StopWords_extract()    # Extract stop words
     twenty_train, X_train_tfidf, X_train_counts, train_count_vect = TFIDF(categories,'train',stop_words,NLTK_StopWords)
     twenty_test, X_test_tfidf, X_test_counts = TFIDF_transform(categories,'test',stop_words,NLTK_StopWords, train_count_vect)
 
-    print X_train_tfidf.shape
-    print X_test_tfidf.shape
+    # print X_train_tfidf.shape
+    # print X_test_tfidf.shape
 
     svd = TruncatedSVD(n_components=50, random_state=42, algorithm='arpack')    # default algorithm will cause segfault
     X_train_LSI = svd.fit_transform(X_train_tfidf)  # fit LSI using X_train_tfidf and perform dimension reduction
@@ -60,31 +59,37 @@ if __name__ == "__main__":
     X_test_LSI = svd.transform(X_test_tfidf)
     X_test_LSI = Normalizer(copy=False).fit_transform(X_test_LSI)
 
-    Y_data_labeling(twenty_test.target) # Category values for verification
-    Y_data_labeling(twenty_train.target) # Category values for trainning
+    clf_OVO = svm.SVC(kernel='linear', probability=True, decision_function_shape = 'ovo')
+    OVO_model = clf_OVO.fit(X_train_LSI, twenty_train.target) # train the data over high-dimension data and known Y values
 
-    clf = svm.SVC(kernel='linear', probability=True)
-    LSI_model = clf.fit(X_train_LSI, twenty_train.target) # train the data over high-dimension data and known Y values
+    clf_OVR = svm.SVC(kernel='linear', probability=True, decision_function_shape = 'ovr')
+    OVR_model = clf_OVR.fit(X_train_LSI, twenty_train.target) # train the data over high-dimension data and known Y values    
 
-    Y_predict = LSI_model.predict(X_test_LSI) # predict the value with the learning algorithm
+    
+    clf_NB = GaussianNB()
+    NB_model = clf_NB.fit(X_train_LSI, twenty_train.target) # train the data over high-dimension data and known Y values 
+
+    # predict the value with the learning algorithm
+    Y_predict_ovo = OVO_model.predict(X_test_LSI) 
+    Y_predict_ovr = OVR_model.predict(X_test_LSI)
+    Y_predict_nb = NB_model.predict(X_test_LSI)
+    Y_predict = [Y_predict_ovo, Y_predict_ovr, Y_predict_nb]
 
     # Calculating the accuracy, recall, precision and confusion matrix
-    accuracy_svm = np.mean(twenty_test.target == Y_predict)
-    print 'The accuracy for the model is %f' % accuracy_svm
-    print '\'0\' is Computer Technology and \'1\' is Recreational Activity'
-    print "The precision and recall values are:"
-    print metrics.classification_report(twenty_test.target, Y_predict)
-    print 'The confusion matrix is as shown below:'
-    print metrics.confusion_matrix(twenty_test.target, Y_predict)
+    accuracy = np.zeros(3)
+    for i in range(len(Y_predict)):
+        accuracy[i] = np.mean(twenty_test.target == Y_predict[i])
 
-    # Plotting the ROC
-    probas_ = LSI_model.predict_proba(X_test_LSI)
-    fpr, tpr, thresholds = metrics.roc_curve(twenty_test.target, probas_[:, 1])
-    plt.plot(fpr, tpr, lw=1, label="SVM ROC")
-    plt.xlim([-0.05, 1.05])
-    plt.ylim([-0.05, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic Example')
-    plt.legend(loc="lower right")
-    plt.show()
+    model_name = ['SVM_OVO ', 'SVM_OVR ', 'NB ']
+
+
+    for i in range(len(accuracy)):
+        s = 'The accuracy for the ' + model_name[i] + 'model is ' + str(accuracy[i])
+        print(s)
+        print ('\'0\' is comp.sys.ibm.pc.hardware, \'1\' is comp.sys.mac.hardware, \'2\' is misc.forsale and \'3\' is soc.religion.christian')
+        print ("The precision and recall values are:")
+        print (metrics.classification_report(twenty_test.target, Y_predict[i]))
+        print ('The confusion matrix is as shown below:')
+        print (metrics.confusion_matrix(twenty_test.target, Y_predict[i]))
+        print("\n")
+
